ad515cecb3f91613b2f70d5c5b2a1eeba8c41feb
unknown <c.derrig@yahoo>
2019-10-20 11:05:19 -0400

    Update to Lab2 with more validation

--- Program.cs
+++ Program.cs
@@ -10,20 +10,20 @@ namespace Lab2
             bool repeat = true;
             decimal length1 = 0.0m;
             decimal width1 = 0.0m;
+            decimal height1 = 0.0m;
 
             Console.WriteLine("Welcome to the Room Dimension Generator");
-
             while (repeat)
             {
                 while (repeat)
                 {
-                    Console.WriteLine("\nHi! Please enter your value for length?");
+                    Console.WriteLine("\nHi! Please enter an numeric value for LENGTH?");
                     string input1 = Console.ReadLine();
                     decimal.TryParse(input1, out length1);
-
                     decimal num1 = 0.0m;
                     if (!decimal.TryParse(input1, out num1))
                     {
+
                         Console.WriteLine("Sorry, please enter a number");
                     }
                     else
@@ -32,13 +32,11 @@ namespace Lab2
                     }
                 }
                 repeat = true;
-
                 while (repeat)
                 {
-                    Console.WriteLine("\nHi! Please enter your value for width?");
+                    Console.WriteLine("\nHi! Please enter a numeric value for WIDTH?");
                     string input2 = Console.ReadLine();
                     decimal.TryParse(input2, out width1);
-
                     decimal num2 = 0.0m;
                     if (!decimal.TryParse(input2, out num2))
                     {
@@ -49,34 +47,53 @@ namespace Lab2
                         repeat = false;
                     }
                 }
+                repeat = true;
+                while (repeat)
+                {
+                    Console.WriteLine("\nHi! Please enter a numeric value for HEIGHT?");
+                    string input2 = Console.ReadLine();
+                    decimal.TryParse(input2, out height1);
+                    decimal num3 = 0.0m;
+                    if (!decimal.TryParse(input2, out num3))
+                    {
+                        Console.WriteLine("Sorry, please enter a number");
+                    }
+                    else
+                    {
+                        repeat = false;
+                    }
+                }
                 Console.WriteLine("\nThanks!, The PERIMETER if the room is " + ((length1 * 2) + (width1 * 2)));
                 Console.WriteLine("         The AREA of the room is " + ((length1) * (width1)));
+                Console.WriteLine("         The VOLUME of the room is " + ((length1) * (width1) * (height1)));
                 Console.WriteLine("\n\nWould you like to run the program again. ('Y' or 'N')\n");
                 string doAgain = Console.ReadLine();
-
                 while (tryAgain)
                 {
-                    if (string.IsNullOrEmpty(doAgain))
-                    {
-                        Console.WriteLine("Please enter either 'Y' or 'N'");
-                        doAgain = Console.ReadLine();
-                    }
-
                     if (doAgain == "n" || doAgain == "N")
                     {
                         Console.WriteLine("\nThanks for the chat.  Have a great day!");
                         return;
                     }
-                    tryAgain = false;
+                    else if (doAgain == "y" || doAgain == "Y")
+                    {
+                        tryAgain = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either 'Y' or 'N'");
+                        doAgain = Console.ReadLine();
+                        tryAgain = true;
+                    }
                 }
+                tryAgain = true;
                 if (doAgain == "y" || doAgain == "Y")
                 {
-
                 }
                 repeat = true;
-                tryAgain = true;
             }
         }
-    } 
+    }
 }
+
 
